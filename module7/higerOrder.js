1feea8070cd74edc7ea3bfbfdc7755967e5caf04
vashistavallambhatla <88158124+vashistavallambhatla@users.noreply.github.com>
2024-12-05 12:18:02 +0530

    Update higerOrder.js

--- module7/higerOrder.js
+++ module7/higerOrder.js
@@ -1,30 +1,30 @@
 function createStepIterator(start, end, step) {
-  let currentValue = start;
-  
-  // Custom iterator object
-  return {
-    next() {
-      if (currentValue <= end) {
-        let value = currentValue;
-        currentValue += step;
-        return { value, done: false };
-      }
-      return { done: true };
-    },
+  return function() {
+    let currentValue = start;
 
-    // Method to return the whole array using the iterator
-    collect() {
-      let result = [];
-      let current = this.next();
-      while (!current.done) {
-        result.push(current.value);
-        current = this.next();
+    return {
+      next() {
+        if (currentValue <= end) {
+          let value = currentValue;
+          currentValue += step;
+          return { value, done: false };
+        }
+        return { done: true };
+      },
+      collect() {
+        let result = [];
+        let current = this.next();
+        while (!current.done) {
+          result.push(current.value);
+          current = this.next();
+        }
+        return result;
       }
-      return result;
-    }
+    };
   };
 }
 
 // Example usage
-const iterator = createStepIterator(1, 10, 2);
+const iterator = createStepIterator(1, 10, 2)();
 console.log(iterator.collect()); // Output: [1, 3, 5, 7, 9]
+
